2606f9d5bd078fe476b3d10676af1154e45f8510
Brian Done <briguy2078@gmail.com>
2022-01-14 15:52:46 -0500

    added comment model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations - A user can make many posts - One to Many relationship
 User.hasMany(Post, {
@@ -11,5 +13,59 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
   });
 
-module.exports = { User, Post };
+// User belongs to many Posts through Vote
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
+
+// Post belongs to many Users through Vote
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+}); 
+
+// Vote belongs to User
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// Vote belongs to Post
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+// User has many Votes
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+// Post has many Votes
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+// Comment belongs to User
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+// Comment belongs to Post
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+// User has many Comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+// Post has many Comments
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
 
