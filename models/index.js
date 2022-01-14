7139d6cefed497c611cabf26847eb1121faf3a09
Brian Done <briguy2078@gmail.com>
2022-01-14 14:03:42 -0500

    added post routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations - A user can make many posts - One to Many relationship
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+// Post has a constraint - only One User
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
 module.exports = { User, Post };
 
